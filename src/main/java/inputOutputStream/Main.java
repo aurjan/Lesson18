ca7c9ffc17736440ff12db0563a6a617c921780b
Aurimas <Aurimas>
2018-03-14 18:49:53 +0200

    first commit

---
+++ src/main/java/inputOutputStream/Main.java
@@ -0,0 +1,9 @@
+package inputOutputStream;
+
+import java.io.FileOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+       // FileOutputStream fileOutputStream = new FileOutputStream("file.txt");
+    }
+}
